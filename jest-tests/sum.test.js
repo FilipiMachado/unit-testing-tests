ffb8585e173bbf38bb2e3fe1193053dedff7b1d4
fil <filipisee@hotmail.com>
2022-08-16 23:39:36 -0300

    testing matchers

--- jest-tests/sum.test.js
+++ jest-tests/sum.test.js
@@ -1,5 +1,5 @@
 const sum = require('./sum')
 
-test('adds 1 + 2 + 3 not to equal 5', () => {
-  expect(sum(1, 2, 3)).not.toBe(5)
+test('adds (1 + 2) - 3 to equal 0', () => {
+  expect(sum(1, 2, 3)).toBe(0)
 })
