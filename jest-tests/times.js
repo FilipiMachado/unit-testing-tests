12d97da074e76774c13ffca2e129c032e2d0f4ec
fil <filipisee@hotmail.com>
2022-08-16 23:14:38 -0300

    testing from zero

--- jest-tests/times.js
+++
@@ -1,5 +0,0 @@
-function times(a, b) {
-  return a * b;
-}
-
-module.exports = times;
--- jest-tests/times.test.js
+++
@@ -1,5 +0,0 @@
-const times = require('./times');
-
-test('5 times 5 to equal 25', () => {
-  expect(times(5, 5)).toBe(25);
-});
