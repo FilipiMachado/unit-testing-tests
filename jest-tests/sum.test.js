cdcca4def8f20db62116e6e21bc4236113934d97
fil <filipisee@hotmail.com>
2022-08-17 20:42:40 -0300

    testing expect.extends

--- jest-tests/sum.test.js
+++ jest-tests/sum.test.js
@@ -62,3 +62,33 @@ test('the best wine flavor is white grape', () => {
   expect(bestWineFlavor('white grape')).toBe('white grape')
 }) */
 
+// EXPECT.EXTEND(MATCHERS)
+
+expect.extend({
+  toBeWithinRange(received, floor, ceiling) {
+    const pass = received >= floor && received <= ceiling
+    console.log(pass)
+    if (pass) {
+      return {
+        message: () =>
+          `expected ${received} not to be within range ${floor} - ${ceiling}`,
+        pass: true,
+      }
+    } else {
+      return {
+        message: () =>
+          `expected ${received} to be within range ${floor} - ${ceiling}`,
+        pass: false,
+      };
+    }
+  },
+});
+
+test('numeric ranges', () => {
+  expect(100).toBeWithinRange(90, 110);
+  expect(101).not.toBeWithinRange(0, 100);
+  expect({ apples: 6, bananas: 3 }).toEqual({
+    apples: expect.toBeWithinRange(1, 10),
+    bananas: expect.not.toBeWithinRange(11, 20)
+  });
+});
