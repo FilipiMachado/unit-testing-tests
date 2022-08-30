7b7630003a535a62c949c58d24f78ca409e135b1
fil <filipisee@hotmail.com>
2022-08-30 20:24:45 -0300

    finished jest tests

--- vue-tests/jest-vue/src/components/search-input/__tests__/search-input.spec.js
+++ vue-tests/jest-vue/src/components/search-input/__tests__/search-input.spec.js
@@ -11,4 +11,31 @@ describe('search-input - Unit', () => {
 
     expect(wrapper.vm).toBeDefined()
   })
+
+  it('should update searchQuery when prop value changes', async () => {
+    const wrapper = mount(SearchInput, {
+      propsData: {
+        value: '',
+      }
+    })
+
+    await wrapper.setProps({ value: 'test' })
+    await wrapper.vm.$nextTick()
+
+    expect(wrapper.vm.searchQuery).toEqual('test')
+  })
+
+  it('should emit input eventwhen something is typed', async () => {
+    const wrapper = mount(SearchInput, {
+      propsData: {
+        value: '',
+      }
+    })
+
+    const inputEl = wrapper.find('input[type=text]')
+    await inputEl.setValue('test')
+    
+    expect(wrapper.emitted().input).toBeTruthy()
+    expect(wrapper.emitted().input[0]).toEqual(['test'])
+  })
 })
